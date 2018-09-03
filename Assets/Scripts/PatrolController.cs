b49ceff29b4578cc9f83ad348f640dc50e1b65f3
Viraj Kaushalye <virajkaush@gmail.com>
2018-09-03 06:51:25 +0530

    Initial Commit

---
+++ Assets/Scripts/PatrolController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolController : MonoBehaviour
+{
+	public List<Transform> patrolPoints;
+	private int currentPoint;
+	private NavMeshAgent _myAgent;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_myAgent = GetComponent<NavMeshAgent>();
+		SetDestination();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_myAgent.remainingDistance <= _myAgent.stoppingDistance)
+		{
+			if (patrolPoints.Count > currentPoint+1)
+			{
+				currentPoint++;
+			}
+			else
+			{
+				currentPoint = 0;
+			}
+			SetDestination();
+		}
+
+		
+	}
+
+	private void SetDestination()
+	{
+		_myAgent.SetDestination(patrolPoints[currentPoint].position);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+	private NavMeshAgent _myAgent;
+
+	void Awake ()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		_myAgent = GetComponent<NavMeshAgent>();	    
+	}
+
+	void FixedUpdate ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(MouseButton.LeftMouse.GetHashCode()))
+		{
+			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(mouseRay, out hit))
+			{
+				_myAgent.SetDestination(hit.point);
+			}
+		}
+	}
+}
